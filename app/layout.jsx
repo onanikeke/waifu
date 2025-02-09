5a02a4ffdcd780dd100e43639def1a2de93fb876
onanikeke <115920647+onanikeke@users.noreply.github.com>
2025-02-09 21:45:28 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,37 +18,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {/* <ProgressBarWithSuspense /> */}
         {children}
-        <div id='fixedban' style='width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);'>
-<div style='text-align:center;display:block;max-width:52px;height:auto;overflow:hidden;margin:auto'>
-<a id='close-fixedban' onclick='document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;' style='cursor:pointer;'><img alt='close' src='https://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif' style='vertical-align:middle;' title='close button'/></a>
-</div>
-<div style='text-align:center;display:block;max-width:300px;height:auto;overflow:hidden;margin:auto'>
-<script type="text/javascript">
-	atOptions = {
-		'key' : '28d616e95ce1677a8725f96915121cad',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/28d616e95ce1677a8725f96915121cad/invoke.js"></script>
-
-</div>
-</div>
- 
- <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4831986,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4831986&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
+        
       </body>
     </html>
   )
